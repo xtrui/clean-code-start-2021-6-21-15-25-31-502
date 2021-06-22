cbc358119f26067ed4d7f60e77d18b1c983beecf
Ray Wen <wenra2@oocl.com>
2021-06-22 15:55:18 +0800

    [refactor] add address class

---
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
@@ -0,0 +1,22 @@
+package com.tw.academy.basic.$6_primitive_obsession.practiceOne;
+
+public class Address {
+    String city;
+    String province;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+}
