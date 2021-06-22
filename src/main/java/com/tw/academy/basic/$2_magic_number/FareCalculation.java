2833ab58a21dad1a9949c0dfd90215e5c13c3a88
Ray Wen <wenra2@oocl.com>
2021-06-22 13:56:34 +0800

    [refactor] magic number

--- src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
+++ src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
@@ -2,13 +2,28 @@ package com.tw.academy.basic.$2_magic_number;
 
 public class FareCalculation {
 
+    public static final int EIGHT_DISCOUNT_START_EXPENSES = 100;
+    public static final int EIGHT_DISCOUNT_END = 150;
+    public static final int FIVE_DISCOUNT_START_EXPENSES = 150;
+    public static final int FIVE_DISCOUNT_END = 400;
+    public static final double EIGHT_DISCOUNT = 0.8;
+    public static final double FIVE_DISCOUNT = 0.5;
+
     public double calculate(double originalPrice, double cumulativeExpensesThisMonth) {
-        if (cumulativeExpensesThisMonth >= 100 && cumulativeExpensesThisMonth < 150) {
-            return originalPrice * 0.8;
+        if (isExpensesInEightDiscount(cumulativeExpensesThisMonth)) {
+            return originalPrice * EIGHT_DISCOUNT;
         }
-        if (cumulativeExpensesThisMonth >= 150 && cumulativeExpensesThisMonth < 400) {
-            return originalPrice * 0.5;
+        if (isExpensesInFiveDiscount(cumulativeExpensesThisMonth)) {
+            return originalPrice * FIVE_DISCOUNT;
         }
         return originalPrice;
+    }
+
+    private boolean isExpensesInFiveDiscount(double cumulativeExpensesThisMonth) {
+        return cumulativeExpensesThisMonth >= FIVE_DISCOUNT_START_EXPENSES && cumulativeExpensesThisMonth < FIVE_DISCOUNT_END;
+    }
+
+    private boolean isExpensesInEightDiscount(double cumulativeExpensesThisMonth) {
+        return cumulativeExpensesThisMonth >= EIGHT_DISCOUNT_START_EXPENSES && cumulativeExpensesThisMonth < EIGHT_DISCOUNT_END;
     }
 }
