4e55fff3f0839d04d02a322cbb0fcb330a80c634
Ray Wen <wenra2@oocl.com>
2021-06-22 22:41:27 +0800

    [refactor] extract magic magic number

---
+++ src/main/java/com/tw/academy/basic/$7_long_method/OrderReceiptConstant.java
@@ -0,0 +1,10 @@
+package com.tw.academy.basic.$7_long_method;
+
+public class OrderReceiptConstant {
+    public final static String TAB = "\t";
+    public final static String NEW_LINE = "\n";
+
+    public final static String PRINT_ORDERS = "======Printing Orders======";
+    public final static String SALES_TAX = "Sales Tax";
+    public final static String TOTAL_AMOUNT = "Total Amount";
+}
--- src/main/java/com/tw/academy/basic/$7_long_method/README.md
+++ src/main/java/com/tw/academy/basic/$7_long_method/README.md
@@ -1,4 +1,5 @@
 * naming
 * deadCode
 * feature envy
+* magic number
 
